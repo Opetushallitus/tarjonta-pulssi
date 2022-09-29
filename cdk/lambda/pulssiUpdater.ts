2e7da4764efa05a77a46f1d6b5e28150d72c0cc3
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-09-29 08:27:23 +0300

    Refaktoroitu pulssiClient tekemään kaikkien entiteettien päivitys kantaan samassa transaktiossa

--- cdk/lambda/pulssiUpdater.ts
+++ cdk/lambda/pulssiUpdater.ts
@@ -2,7 +2,6 @@ import { Handler } from "aws-lambda";
 import { Pool, PoolClient } from "pg";
 import { Client as ElasticClient } from "@elastic/elasticsearch";
 import { getSSMParam, entityTypes, DEFAULT_DB_POOL_PARAMS } from "./shared";
-import type { EntityType } from "./shared";
 
 const PULSSI_DB_USER = await getSSMParam(
   process.env.TARJONTAPULSSI_POSTGRES_APP_USER
@@ -32,118 +31,117 @@ const connectElastic = async () => {
 
 const elasticClient = await connectElastic();
 
-const queryCountsFromElastic = async (entity: EntityType) => {
-  const aggs = {
-    by_tila: {
-      terms: {
-        field: "tila.keyword",
-        size: 10,
-      },
-      aggs:
-        entity === "haku"
-          ? {
-              by_hakutapa: {
-                terms: {
-                  field: "hakutapa.koodiUri.keyword",
-                  size: 100,
-                },
-              },
-            }
-          : {
-              by_koulutustyyppi_path: {
-                terms: {
-                  field: "koulutustyyppiPath.keyword",
-                  size: 100,
-                },
-              },
+const queryCountsFromElastic = async () =>
+  elasticClient.msearch({
+    body: entityTypes.flatMap((entity) => [
+      { index: `${entity}-kouta` },
+      {
+        size: 0, // Ei haluta hakutuloksia, vain aggsit
+        track_total_hits: true, // Halutaan aina tarkka hits-määrä, eikä jotain sinne päin
+        query: {
+          terms: {
+            tila: ["julkaistu", "arkistoitu"],
+          },
+        },
+        aggs: {
+          by_tila: {
+            terms: {
+              field: "tila.keyword",
+              size: 10,
             },
-    },
-  };
-
-  return await elasticClient.search({
-    index: `${entity}-kouta`,
-    body: {
-      _source: false, // Halutaan vain aggsit, ei _source:a
-      size: 0, // ...eikä hitsejä
-      track_total_hits: true, // Halutaan aina tarkka hits-määrä, eikä jotain sinne päin
-      query: {
-        terms: {
-          tila: ["julkaistu", "arkistoitu"],
+            aggs:
+              entity === "haku"
+                ? {
+                    by_hakutapa: {
+                      terms: {
+                        field: "hakutapa.koodiUri.keyword",
+                        size: 100,
+                      },
+                    },
+                  }
+                : {
+                    by_koulutustyyppi_path: {
+                      terms: {
+                        field: "koulutustyyppiPath.keyword",
+                        size: 100,
+                      },
+                    },
+                  },
+          },
         },
       },
-      aggs,
-    },
+    ]),
   });
-};
 
-const countsFromElasticToDb = async (
-  pulssiClient: PoolClient,
-  entity: EntityType
-) => {
-  const elasticRes = await queryCountsFromElastic(entity);
+const countsFromElasticToDb = async (pulssiClient: PoolClient) => {
+  const msearchRes = await queryCountsFromElastic();
 
-  const tilaBuckets = elasticRes.body?.aggregations?.by_tila?.buckets ?? [];
-
-  const subAggName =
-    entity === "haku" ? "by_hakutapa" : "by_koulutustyyppi_path";
-
-  const subAggColumn = entity === "haku" ? "hakutapa" : "tyyppi_path";
-
-  // TODO: Ajetaan kaikkien entiteettien datan päivitykset yhdessä transaktiossa
   try {
     await pulssiClient.query("BEGIN");
-    const rows = (
-      await pulssiClient.query(
-        `select tila, ${subAggColumn} from ${entity}_amounts group by (tila, ${subAggColumn})`
-      )
-    )?.rows;
-    // Asetetaan nollaksi aggregaatioiden luvut, jotka löytyy kannasta, mutta ei elasticista.
-    // Bucketteja voi kadota, jos entiteettejä muokataan. Tarvitsee nollata, jotta kantaan ei jää haamu-lukuja sotkemaan.
-    rows.forEach((row) => {
-      const subBuckets = tilaBuckets?.find(
-        (v: { key: string }) => v.key === row.tila
-      )?.buckets;
-      const subBucket = subBuckets?.find(
-        (v: { key: string }) => v.key === row?.[subAggColumn]
-      );
-      if (Array.isArray(subBuckets) && !subBucket) {
-        subBuckets.push({
-          key: row[subAggColumn],
-          doc_count: 0,
-        });
-      }
-    });
+    for (let index = 0; index < entityTypes.length; ++index) {
+      const entity = entityTypes[index];
+      const elasticResBody = msearchRes.body.responses?.[index];
 
-    for (const tilaBucket of tilaBuckets) {
-      const tila = tilaBucket.key;
-      const subBuckets = tilaBucket?.[subAggName]?.buckets ?? [];
+      const tilaBuckets = elasticResBody?.aggregations?.by_tila?.buckets ?? [];
 
-      for (const subBucket of subBuckets) {
-        const amount = subBucket.doc_count ?? 0;
+      const subAggName =
+        entity === "haku" ? "by_hakutapa" : "by_koulutustyyppi_path";
 
-        const existingRow = (
-          await pulssiClient.query(
-            `SELECT * from ${entity}_amounts WHERE tila = '${tila}' AND ${subAggColumn} = '${subBucket.key}'`
-          )
-        )?.rows?.[0];
+      const subAggColumn = entity === "haku" ? "hakutapa" : "tyyppi_path";
 
-        if (existingRow) {
-          // Ei päivitetä kantaa, jos luku ei ole muuttunut!
-          if (Number(existingRow.amount) !== Number(amount)) {
+      const rows = (
+        await pulssiClient.query(
+          `select tila, ${subAggColumn} from ${entity}_amounts group by (tila, ${subAggColumn})`
+        )
+      )?.rows;
+      // Asetetaan nollaksi aggregaatioiden luvut, jotka löytyy kannasta, mutta ei elasticista.
+      // Bucketteja voi kadota, jos entiteettejä muokataan. Tarvitsee nollata, jotta kantaan ei jää haamu-lukuja sotkemaan.
+      rows.forEach((row) => {
+        const subBuckets = tilaBuckets?.find(
+          (v: { key: string }) => v.key === row.tila
+        )?.buckets;
+        const subBucket = subBuckets?.find(
+          (v: { key: string }) => v.key === row?.[subAggColumn]
+        );
+        if (Array.isArray(subBuckets) && !subBucket) {
+          subBuckets.push({
+            key: row[subAggColumn],
+            doc_count: 0,
+          });
+        }
+      });
+
+      for (const tilaBucket of tilaBuckets) {
+        const tila = tilaBucket.key;
+        const subBuckets = tilaBucket?.[subAggName]?.buckets ?? [];
+
+        for (const subBucket of subBuckets) {
+          const amount = subBucket.doc_count ?? 0;
+
+          const existingRow = (
+            await pulssiClient.query(
+              `SELECT * from ${entity}_amounts WHERE tila = '${tila}' AND ${subAggColumn} = '${subBucket.key}'`
+            )
+          )?.rows?.[0];
+
+          if (existingRow) {
+            // Ei päivitetä kantaa, jos luku ei ole muuttunut!
+            if (Number(existingRow.amount) !== Number(amount)) {
+              console.log(
+                `Updating changed ${entity} amount: ${tila}, ${subBucket.key} = ${amount}`
+              );
+              await pulssiClient.query(
+                `UPDATE ${entity}_amounts SET amount = ${amount} WHERE tila = '${tila}' AND ${subAggColumn} = '${subBucket.key}'`
+              );
+            }
+          } else {
             console.log(
-              `Updating changed ${entity} amount: ${tila}, ${subBucket.key} = ${amount}`
+              `Inserting ${entity} amount: ${tila}, ${subBucket.key} = ${amount}`
             );
             await pulssiClient.query(
-              `UPDATE ${entity}_amounts SET amount = ${amount} WHERE tila = '${tila}' AND ${subAggColumn} = '${subBucket.key}'`
+              `INSERT INTO ${entity}_amounts(${subAggColumn}, tila, amount) values('${subBucket.key}', '${tila}', ${amount})`
             );
           }
-        } else {
-          console.log(
-            `Inserting ${entity} amount: ${tila}, ${subBucket.key} = ${amount}`
-          );
-          await pulssiClient.query(
-            `INSERT INTO ${entity}_amounts(${subAggColumn}, tila, amount) values('${subBucket.key}', '${tila}', ${amount})`
-          );
         }
       }
     }
@@ -162,9 +160,7 @@ export const main: Handler = async (event, context, callback) => {
   const pulssiClient = await pulssiDbPool.connect();
 
   try {
-    return await Promise.allSettled(
-      entityTypes.map((entity) => countsFromElasticToDb(pulssiClient, entity))
-    );
+    return await countsFromElasticToDb(pulssiClient);
   } finally {
     // https://github.com/brianc/node-postgres/issues/1180#issuecomment-270589769
     pulssiClient.release(true);
