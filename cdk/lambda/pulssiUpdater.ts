aec965c25f78a1d565b2851be4be0f3f088d75d5
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-09-29 08:27:23 +0300

    Tallennetaan updater-lambdassa luvut elasticista pulssin kantaan

--- cdk/lambda/pulssiUpdater.ts
+++ cdk/lambda/pulssiUpdater.ts
@@ -15,7 +15,7 @@ const getSSMParam = async (param?: string) => {
     const result = await ssm
       .getParameter({
         Name: param,
-        WithDecryption: true
+        WithDecryption: true,
       })
       .promise();
     return result.Parameter?.Value;
@@ -29,7 +29,7 @@ const DEFAULT_DB_POOL_PARAMS = {
   max: 1,
   min: 0,
   idleTimeoutMillis: 120000,
-  connectionTimeoutMillis: 10000
+  connectionTimeoutMillis: 10000,
 };
 const connectKoutaDb = async () => {
   const KOUTA_DB_USER = await getSSMParam(process.env.KOUTA_POSTGRES_RO_USER);
@@ -43,7 +43,7 @@ const connectKoutaDb = async () => {
     port: 5432,
     database: "kouta",
     user: KOUTA_DB_USER,
-    password: KOUTA_DB_PASSWORD
+    password: KOUTA_DB_PASSWORD,
   });
 
   return pool.connect();
@@ -63,7 +63,7 @@ const connectPulssiDb = async () => {
     port: 5432,
     database: "tarjontapulssi",
     user: PULSSI_DB_USER,
-    password: PULSSI_DB_PASSWORD
+    password: PULSSI_DB_PASSWORD,
   });
 
   return pool.connect();
@@ -75,18 +75,22 @@ const connectElastic = async () => {
   );
 
   return new ElasticClient({
-    node: ELASTIC_URL_WITH_CREDENTIALS
+    node: ELASTIC_URL_WITH_CREDENTIALS,
   });
 };
 
-type EntityType = "koulutus" | "toteutus" | "hakukohde" | "haku";
+const pulssiClient = await connectPulssiDb();
+const elasticClient = await connectElastic();
 
-const getCounts = async (elasticClient: ElasticClient, entity: EntityType) => {
+const entityTypes = ["koulutus", "toteutus", "hakukohde", "haku"] as const;
+type EntityType = typeof entityTypes[number];
+
+const queryCountsFromElastic = async (entity: EntityType) => {
   const aggs = {
     by_tila: {
       terms: {
         field: "tila.keyword",
-        size: 10
+        size: 10,
       },
       aggs:
         entity === "haku"
@@ -94,22 +98,22 @@ const getCounts = async (elasticClient: ElasticClient, entity: EntityType) => {
               by_hakutapa: {
                 terms: {
                   field: "hakutapa.koodiUri.keyword",
-                  size: 100
-                }
-              }
+                  size: 100,
+                },
+              },
             }
           : {
               by_koulutustyyppi_path: {
                 terms: {
                   field: "koulutustyyppiPath.keyword",
-                  size: 100
-                }
-              }
-            }
-    }
+                  size: 100,
+                },
+              },
+            },
+    },
   };
 
-  const res = await elasticClient.search({
+  return await elasticClient.search({
     index: `${entity}-kouta`,
     body: {
       _source: false, // Halutaan vain aggsit, ei _source:a
@@ -117,12 +121,16 @@ const getCounts = async (elasticClient: ElasticClient, entity: EntityType) => {
       track_total_hits: true, // Halutaan aina tarkka hits-määrä, eikä jotain sinne päin
       query: {
         terms: {
-          tila: ["julkaistu", "arkistoitu"]
-        }
+          tila: ["julkaistu", "arkistoitu"],
+        },
       },
-      aggs
-    }
+      aggs,
+    },
   });
+};
+
+const getCounts = async (entity: EntityType) => {
+  const res = await queryCountsFromElastic(entity);
 
   const tilaBuckets = res.body?.aggregations?.by_tila?.buckets ?? [];
 
@@ -136,7 +144,7 @@ const getCounts = async (elasticClient: ElasticClient, entity: EntityType) => {
 
       if (entity === "haku") {
         acc[node.key] = {
-          _count: count
+          _count: count,
         };
       } else {
         const ktParts = node.key.split("/");
@@ -148,7 +156,7 @@ const getCounts = async (elasticClient: ElasticClient, entity: EntityType) => {
           }
           if (!acc[part]) {
             acc[part] = {
-              _count: 0
+              _count: 0,
             };
           }
           acc[part]._parent = previousPart;
@@ -159,31 +167,101 @@ const getCounts = async (elasticClient: ElasticClient, entity: EntityType) => {
     }, {});
     result[tilaAgg.key] = {
       _count: tilaAgg.doc_count,
-      ...countsByTyyppi
+      ...countsByTyyppi,
     };
     return result;
   }, {});
 
   return {
     _count: res?.body?.hits?.total?.value ?? 0,
-    ...countsByTila
+    ...countsByTila,
   };
 };
 
-//const koutaClient = await connectKoutaDb()
-const elasticClient = await connectElastic();
+const countsFromElasticToDb = async (entity: EntityType) => {
+  const elasticRes = await queryCountsFromElastic(entity);
+
+  const tilaBuckets = elasticRes.body?.aggregations?.by_tila?.buckets ?? [];
+
+  const subAggName =
+    entity === "haku" ? "by_hakutapa" : "by_koulutustyyppi_path";
+
+  const subAggColumn = entity === "haku" ? "hakutapa" : "tyyppi_path";
+
+  // TODO: Ajetaan kaikkien entiteettien datan päivitykset yhdessä transaktiossa
+  try {
+    await pulssiClient.query("BEGIN;");
+    const rows = (
+      await pulssiClient.query(
+        `select tila, ${subAggColumn} from ${entity}_amounts group by (tila, ${subAggColumn})`
+      )
+    )?.rows;
+    // Asetetaan nollaksi aggregaatioiden luvut, jotka löytyy kannasta, mutta ei elasticista.
+    // Bucketteja voi kadota, jos entiteettejä muokataan. Tarvitsee nollata, jotta kantaan ei jää haamu-lukuja sotkemaan.
+    rows.forEach((row) => {
+      const subBuckets = tilaBuckets?.find(
+        (v: { key: string }) => v.key === row.tila
+      )?.buckets;
+      const subBucket = subBuckets?.find(
+        (v: { key: string }) => v.key === row?.[subAggColumn]
+      );
+      if (Array.isArray(subBuckets) && !subBucket) {
+        subBuckets.push({
+          key: row[subAggColumn],
+          doc_count: 0,
+        });
+      }
+    });
+
+    for (const tilaBucket of tilaBuckets) {
+      const tila = tilaBucket.key;
+      const subBuckets = tilaBucket?.[subAggName]?.buckets ?? [];
+
+      for (const subBucket of subBuckets) {
+        const amount = subBucket.doc_count ?? 0;
+
+        const existingRow = (
+          await pulssiClient.query(
+            `SELECT * from ${entity}_amounts WHERE tila = '${tila}' AND ${subAggColumn} = '${subBucket.key}'`
+          )
+        )?.rows?.[0];
+
+        if (existingRow) {
+          // Ei päivitetä kantaa, jos luku ei ole muuttunut!
+          if (Number(existingRow.amount) !== Number(amount)) {
+            console.log(
+              `Updating changed ${entity} amount: ${tila}, ${subBucket.key} = ${amount}`
+            );
+            await pulssiClient.query(
+              `UPDATE ${entity}_amounts SET amount = ${amount} WHERE tila = '${tila}' AND ${subAggColumn} = '${subBucket.key}'`
+            );
+          }
+        } else {
+          console.log(
+            `Inserting ${entity} amount: ${tila}, ${subBucket.key} = ${amount}`
+          );
+          await pulssiClient.query(
+            `INSERT INTO ${entity}_amounts(${subAggColumn}, tila, amount) values('${subBucket.key}', '${tila}', ${amount})`
+          );
+        }
+      }
+    }
+    await pulssiClient.query("COMMIT;");
+  } catch (e) {
+    console.log("ROLLBACK");
+    await pulssiClient.query("ROLLBACK;");
+    console.error(e);
+  }
+};
 
 export const main: Handler = async (event, context, callback) => {
   // https://github.com/brianc/node-postgres/issues/930#issuecomment-230362178
   context.callbackWaitsForEmptyEventLoop = false; // !important to use pool
 
   try {
-    return {
-      koulutus: await getCounts(elasticClient, "koulutus"),
-      toteutus: await getCounts(elasticClient, "toteutus"),
-      hakukohde: await getCounts(elasticClient, "hakukohde"),
-      haku: await getCounts(elasticClient, "haku")
-    };
+    return await Promise.allSettled(
+      entityTypes.map((entity) => countsFromElasticToDb(entity))
+    );
   } finally {
     // https://github.com/brianc/node-postgres/issues/1180#issuecomment-270589769
     //koutaClient.release(true);
