344d18b368465515daefb254591d32de8257a27e
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-10-03 16:08:12 +0300

    Noudetaan käännökset viewer-lambdassa ja tallennetaan S3:een JSON-tiedostoon.

--- cdk/lambda/pulssiViewer.ts
+++ cdk/lambda/pulssiViewer.ts
@@ -8,6 +8,7 @@ import {
 } from "./shared";
 import type { EntityType } from "./shared";
 import { Pool } from "pg";
+import fetch from "node-fetch";
 
 const PULSSI_DB_USER = await getSSMParam(
   process.env.TARJONTAPULSSI_POSTGRES_RO_USER
@@ -41,6 +42,38 @@ const getCounts = async (entity: EntityType) => {
   return getPulssiEntityData(res, entity);
 };
 
+const koodistoURI = (koodistoNimi: string) =>
+  `https://${process.env.PUBLICHOSTEDZONE}/koodisto-service/rest/json/${koodistoNimi}/koodi?onlyValidKoodis=true`;
+const localizationsUri = `https://${process.env.PUBLICHOSTEDZONE}/lokalisointi/cxf/rest/v1/localisation?category=tarjonta-pulssi`;
+
+const getKoodistoTranslations = async (koodistoNimi: string) => {
+  return fetch(koodistoURI(koodistoNimi))
+    .then((res) => res.json())
+    .then((data: any) =>
+      data?.reduce((result: any, hakutapa: any) => {
+        result[hakutapa?.koodiUri] = Object.fromEntries(
+          hakutapa.metadata?.map((tr: any) => [tr.kieli.toLowerCase(), tr.nimi])
+        );
+        return result;
+      }, {})
+    );
+};
+
+const getLocalizations = async () => {
+  return fetch(localizationsUri)
+    .then((res) => res.json())
+    .then((data: any) => {
+      const result: Record<string, any> = {};
+      data?.forEach((translation: any) => {
+        if (!result[translation.key]) {
+          result[translation.key] = {};
+        }
+        result[translation.key][translation.locale] = translation.value;
+      });
+      return result;
+    });
+};
+
 export const main: Handler = async (event, context /*, callback*/) => {
   // https://github.com/brianc/node-postgres/issues/930#issuecomment-230362178
   context.callbackWaitsForEmptyEventLoop = false; // !important to reuse pool
@@ -52,5 +85,21 @@ export const main: Handler = async (event, context /*, callback*/) => {
     haut: await getCounts("haku"),
   };
 
-  await putPulssiS3Object({ Key: "pulssi.json", Body: pulssiData, ContentType: "application/json; charset=utf-8" });
+  const translations = {
+    ...(await getLocalizations()),
+    ...(await getKoodistoTranslations("koulutustyyppi")),
+    ...(await getKoodistoTranslations("hakutapa"))
+  };
+
+  await putPulssiS3Object({
+    Key: "pulssi.json",
+    Body: pulssiData,
+    ContentType: "application/json; charset=utf-8",
+  });
+
+  await putPulssiS3Object({
+    Key: "translations.json",
+    Body: translations,
+    ContentType: "application/json; charset=utf-8",
+  });
 };
