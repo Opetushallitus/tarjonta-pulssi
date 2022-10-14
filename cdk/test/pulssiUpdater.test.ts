7d00c2e9937af69ed283e3fdf3ca4f9d73d20c5c
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-10-14 16:02:56 +0300

    Korjattu pulssiUpdater testi

--- cdk/test/pulssiUpdater.test.ts
+++ cdk/test/pulssiUpdater.test.ts
@@ -1,31 +1,32 @@
 import { initializeSubBuckets } from "../lambda/elasticUtils";
 import { RowWithKoulutustyyppiPath } from "../shared/types";
 
-test("getTilaBuckets should reset bucket amounts to zero, if they exist in db response, but not in elastic", () => {
+test("initializeSubBuckets should reset sub-bucket amounts to zero, if they exist in db response, but not in elastic", () => {
   const rows: Array<RowWithKoulutustyyppiPath> = [
     {
       tila: "julkaistu",
       tyyppi_path: "tuva/tuva-normal",
-      amount: 1
+      amount: 1,
     },
   ];
 
   const elasticResBody = {
+    took: 10,
+    timed_out: false,
     aggregations: {
       by_tila: {
+        sum_other_doc_count: 0,
         buckets: [
           {
             key: "arkistoitu",
             doc_count: 1,
-            aggregations: {
-              by_koulutustyyppi_path: {
-                buckets: [
-                  {
-                    key: "tuva/tuva-normal",
-                    doc_count: 1,
-                  },
-                ],
-              },
+            by_koulutustyyppi_path: {
+              buckets: [
+                {
+                  key: "tuva/tuva-normal",
+                  doc_count: 1,
+                },
+              ],
             },
           },
         ],
@@ -33,38 +34,43 @@ test("getTilaBuckets should reset bucket amounts to zero, if they exist in db re
     },
   };
 
-  const expectedTilaBuckets = [
-    {
-      key: "arkistoitu",
-      doc_count: 1,
-      aggregations: {
-        by_koulutustyyppi_path: {
-          buckets: [
-            {
-              key: "tuva/tuva-normal",
-              doc_count: 1,
+  const expectedResBody = {
+    took: 10,
+    timed_out: false,
+    aggregations: {
+      by_tila: {
+        sum_other_doc_count: 0,
+        buckets: [
+          {
+            key: "arkistoitu",
+            doc_count: 1,
+            by_koulutustyyppi_path: {
+              buckets: [
+                {
+                  key: "tuva/tuva-normal",
+                  doc_count: 1,
+                },
+              ],
             },
-          ],
-        },
+          },
+          {
+            key: "julkaistu",
+            doc_count: 0,
+            by_koulutustyyppi_path: {
+              buckets: [
+                {
+                  key: "tuva/tuva-normal",
+                  doc_count: 0,
+                },
+              ],
+            },
+          },
+        ],
       },
     },
-    {
-      key: "julkaistu",
-      doc_count: 0,
-      aggregations: {
-        by_koulutustyyppi_path: {
-          buckets: [
-            {
-              key: "tuva/tuva-normal",
-              doc_count: 0,
-            },
-          ],
-        },
-      },
-    },
-  ];
+  };
 
   expect(
-    initializeSubBuckets(rows, elasticResBody, "by_koulutustyyppi_path")
-  ).toMatchObject(expectedTilaBuckets);
+    initializeSubBuckets(rows, elasticResBody as any, "by_koulutustyyppi_path")
+  ).toMatchObject(expectedResBody);
 });
