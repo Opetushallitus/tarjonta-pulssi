e73c5486aca234009e7834f5c165e30400755d8c
Esa Hakala <esa.hakala@gofore.com>
2023-09-14 14:57:14 +0300

    working environment logic and Remix within VPC

--- my-remix-app/stacks/tarjonta-pulssi.ts
+++ my-remix-app/stacks/tarjonta-pulssi.ts
@@ -1,8 +1,83 @@
-import { StackContext, RemixSite } from "sst/constructs";
+import { StackContext, RemixSite, } from "sst/constructs";
+import { Vpc, SubnetType } from "aws-cdk-lib/aws-ec2";
+// import { Fn } from "aws-cdk-lib";
+
+
+
 
 export function TARJONTAPULSSI({ stack }: StackContext) {
-  const site = new RemixSite(stack, "site");
+  
+// Environment specific configurations
+  let OPHhostedZone
+  let OPHdomainName
+
+  if (stack.stage === "sade")
+  {
+    OPHhostedZone = "opintopolku.fi";
+    OPHdomainName = "tarjonta-pulssi.opintopolku.fi";
+  }
+  else if (stack.stage === "untuva")
+  {
+    OPHhostedZone = "untuvaopintopolku.fi";
+    OPHdomainName = "tarjonta-pulssi.untuvaopintopolku.fi";
+  }
+  else if (stack.stage === "hahtuva")
+  {
+    OPHhostedZone = "hahtuvaopintopolku.fi";
+    OPHdomainName = "tarjonta-pulssi.hahtuvaopintopolku.fi";
+  }
+  else if (stack.stage === "pallero")
+  {
+    OPHhostedZone = "testiopintopolku.fi";
+    OPHdomainName = "tarjonta-pulssi.testiopintopolku.fi";
+  }
+  else
+  {
+    OPHhostedZone = undefined
+    OPHdomainName = undefined
+  }
+
+  console.log("OPHHostedZone value was: " + OPHhostedZone)
+  console.log("OPHdomainName value was: " + OPHdomainName)
+
+  const vpc = Vpc.fromLookup(stack, "myVPC", {
+    vpcName: `opintopolku-vpc-${stack.stage}`
+  });
+
+
+  // const myvpc = Vpc.fromVpcAttributes(stack, "VPC", {
+  //   vpcId: Fn.importValue(`${stack.stage}-Vpc`),
+  //   availabilityZones: [
+  //     Fn.importValue(`${stack.stage}-SubnetAvailabilityZones`),
+  //   ],
+  //   privateSubnetIds: [
+  //     Fn.importValue(`${stack.stage}-PrivateSubnet1`),
+  //     Fn.importValue(`${stack.stage}-PrivateSubnet2`),
+  //     Fn.importValue(`${stack.stage}-PrivateSubnet3`),
+  //   ],
+  // });
+
+
+// Remix site
+  const site = new RemixSite(stack, "site", {
+    customDomain: {
+      domainName: OPHdomainName, 
+      hostedZone: OPHhostedZone,
+    },
+    cdk: {
+      server: {
+        vpc,
+        vpcSubnets: {
+          subnetType: SubnetType.PRIVATE_WITH_NAT,
+        }
+      }
+    }
+  });
+
+  
+
   stack.addOutputs({
-    url: site.url,
+    cloudfronturl: site.url,
+    customurl: site.customDomainUrl
   });
 };
