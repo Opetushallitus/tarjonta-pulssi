1f3c97aa3e5ff859754f97c3d62faf703eab5d0b
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2023-10-04 10:56:00 +0300

    OPHJOD-110: Poistettu vielä vanhan cdk -hakemiston sisältö

--- cdk/test/resources/load_elastic_dump.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run --add-host=host.docker.internal:host-gateway --rm -v $(pwd)/test/resources/elasticdump:/tmp elasticdump/elasticsearch-dump \
-multielasticdump --direction=load --input=/tmp --output=$1 --includeType=$2
