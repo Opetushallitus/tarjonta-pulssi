1bfa1090ebaeee1530dafdf221861abe200bd143
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2023-10-30 12:24:59 +0200

    OPHJOD-110: Poistettu turhat käännös-tiedostot

--- sst-app/packages/frontend/app/hooks/useTranslation.ts
+++
@@ -1,7 +0,0 @@
-import { useTranslation as reactUseTranslation } from "react-i18next";
-
-export const useTranslation = () => {
-  const { t : reactT, i18n } = reactUseTranslation();
-
-  return {t: (key: string) => reactT(`${key}.${i18n.language}`), i18n};
-};
