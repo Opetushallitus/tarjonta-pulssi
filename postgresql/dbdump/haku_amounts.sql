0e49c8c822e07b162652cd2f47bf5032fde54257
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2023-11-02 16:41:21 +0200

    OPHJOD-110: Lisätty testidatan importtaus lokaaliin kantaan

---
+++ postgresql/dbdump/haku_amounts.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_01', 'arkistoitu', 8, 612984, '["2023-10-29 22:06:54.611364+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_06', 'julkaistu', 20, 619969, '["2023-11-02 11:26:54.514139+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_01', 'julkaistu', 4, 612984, '["2023-10-29 22:06:54.611364+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_03', 'arkistoitu', 1094, 620118, '["2023-11-02 13:16:54.512226+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_03', 'julkaistu', 1246, 620118, '["2023-11-02 13:16:54.512226+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_05', 'arkistoitu', 83, 617559, '["2023-11-01 06:26:54.459555+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_05', 'julkaistu', 29, 617559, '["2023-11-01 06:26:54.459555+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_06', 'arkistoitu', 57, 614933, '["2023-10-30 22:06:54.486629+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_02', 'arkistoitu', 635, 619479, '["2023-11-02 06:06:54.470137+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_04', 'arkistoitu', 47, 619479, '["2023-11-02 06:06:54.470137+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_04', 'julkaistu', 37, 619479, '["2023-11-02 06:06:54.470137+00",)');
+INSERT INTO public.haku_amounts (hakutapa, tila, amount, transaction_id, system_time) VALUES ('hakutapa_02', 'julkaistu', 333, 619506, '["2023-11-02 06:26:54.419007+00",)');
