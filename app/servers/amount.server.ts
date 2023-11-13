1f526dde1dd55e4e062c31a3871aa2f70472828e
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-11-13 10:23:44 +0200

    Yksinkertaistettu projektin rakennetta

---
+++ app/servers/amount.server.ts
@@ -0,0 +1,54 @@
+import { Pool } from "pg";
+
+import { getCurrentAmountDataFromDb, getHistoryDataFromDb } from "../../functions/pulssiDbAccessor";
+import { getCombinedHistoryData } from "../../shared/amountDataUtils";
+import { DEFAULT_DB_POOL_PARAMS } from "../../shared/dbUtils";
+import data_current from "../../shared/testdata/pulssi.json";
+import data_old from "../../shared/testdata/pulssi_old.json";
+import type { PulssiData } from "../../shared/types";
+
+const localPulssiDbPool = new Pool({
+  ...DEFAULT_DB_POOL_PARAMS,
+  host: "localhost",
+  database: "tarjontapulssi",
+  user: "oph",
+  password: "oph",
+});
+
+export const getCurrentAmountData = async () => {
+  switch (process.env.DATABASE) {
+    case "file":
+      return data_current;
+    case "local":
+      return await getCurrentAmountDataFromDb(localPulssiDbPool);
+    default: {
+      const results = await fetch(process.env.DB_API_URL || "");
+      const jsonResult = await results.json();
+      return jsonResult;
+    }
+  }
+};
+
+export const getHistoryAmountData = async (startStr: string | null, endStr: string | null) => {
+  switch (process.env.DATABASE) {
+    case "file":
+      return getCombinedHistoryData(
+        data_old as unknown as PulssiData,
+        data_current as unknown as PulssiData
+      );
+    case "local":
+      return await getHistoryDataFromDb(
+        localPulssiDbPool,
+        startStr ?? undefined,
+        endStr ?? undefined
+      );
+    default: {
+      let params = { history: "true " };
+      params = startStr !== null ? Object.assign(params, { start: startStr }) : params;
+      params = endStr !== null ? Object.assign(params, { end: endStr }) : params;
+      const url = `${process.env.DB_API_URL || ""}?${new URLSearchParams(params).toString()}`;
+      const results = await fetch(url);
+      return await results.json();
+    }
+  }
+};
